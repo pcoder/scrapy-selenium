11e4c1aba2b81a607a45a74a6219fd1fc391c47e
PCoder <purple.coder@yahoo.co.uk>
2022-07-31 20:13:25 +0530

    Fix bug
      ╰─> [13 lines of output]
          Ignoring invalid distribution -wsgi (/home/pcoder/projects/quilen/routing-webapp/venv310/lib/python3.10/site-packages)
          Ignoring invalid distribution -ip (/home/pcoder/projects/quilen/routing-webapp/venv310/lib/python3.10/site-packages)
          Ignoring invalid distribution - (/home/pcoder/projects/quilen/routing-webapp/venv310/lib/python3.10/site-packages)
          Traceback (most recent call last):
            File "<string>", line 2, in <module>
            File "<pip-setuptools-caller>", line 34, in <module>
            File "/tmp/pip-req-build-hl7_n613/setup.py", line 31, in <module>
              install_requires=get_requirements('requirements/requirements.txt')
            File "/tmp/pip-req-build-hl7_n613/setup.py", line 26, in get_requirements
              return [str(ir.req) for ir in install_reqs]
            File "/tmp/pip-req-build-hl7_n613/setup.py", line 26, in <listcomp>
              return [str(ir.req) for ir in install_reqs]
          AttributeError: 'ParsedRequirement' object has no attribute 'req'

--- setup.py
+++ setup.py
@@ -22,8 +22,14 @@ def get_requirements(source):
     """
 
     install_reqs = parse_requirements(filename=source, session=PipSession())
+    
+    requirements = list(requirements)
+    try:
+        requirements = [str(ir.req) for ir in install_reqs]
+    except:
+        requirements = [str(ir.requirement) for ir in install_reqs]
 
-    return [str(ir.req) for ir in install_reqs]
+    return requirements
 
 
 setup(
